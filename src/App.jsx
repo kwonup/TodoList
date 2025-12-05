70f713fd29959a9c03b4d2b8846c463a7fb44e14
kwon youngwoo <kwonyoungwoo@kwonui-MacBookAir.local>
2025-12-05 19:34:44 +0900

    Create-todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "웨이트하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "유산소하기",
+    date: new Date().getTime(),
+  },
+];
 function App() {
-  const mockData = [
-    {
-      id: 0,
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
       isDone: false,
-      content: "React 공부하기",
-      date: new Date().getItem(),
-    },
-    {
-      id: 1,
-      isDone: false,
-      content: "웨이트하기",
-      date: new Date().getItem(),
-    },
-    {
-      id: 2,
-      isDone: false,
-      content: "유산소하기",
-      date: new Date().getItem(),
-    },
-  ];
-  const [todos, setTodos] = useState([]);
+      content: content,
+      date: new Date().getTime(),
+    };
+    setTodos([newTodo, ...todos]);
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,9 +1,38 @@
+import { useState, useRef } from "react";
 import "./Editor.css";
-const Editor = () => {
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+  //엔터 누르면 데이터 추가
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      //keycode 13은 엔터 의미
+      onSubmit();
+    }
+  };
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent(""); //추가하고 입력창 초기화
+  };
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
